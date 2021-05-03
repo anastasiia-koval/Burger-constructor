9d82f98e5f18068f69a2322aeac83fcecff185c1
anastasiia-koval <anastasiia.koval05@gmail.com>
2021-05-03 16:26:52 +0200

    fixed imports in App file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import Layout from './components/Layout/Layout';
+import Layout from '../src/container/Layout/Layout';
 // import BurgerBuilder from './components/BurgerBuilder/BurgerBuilder'
 
 function App() {
